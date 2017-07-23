a0c7ee1f5d5d37144b99819e84da506754cac3ad
Jeremy Whiteley <jeremy@medtexter.com>
2017-07-22 22:00:08 -0700

    sfsafs

--- TodoDocumentDB/Constants.cs
+++ TodoDocumentDB/Constants.cs
@@ -4,7 +4,7 @@
 	{
 		// Azure DocumentDB
 		public static readonly string EndpointUri = "https://xamcosmosdb.documents.azure.com:443/";
-		public static readonly string DatabaseName = "Todolist";
+		public static readonly string DatabaseName = "TodoList";
 		public static readonly string CollectionName = "UserItems";
 
 		public static string ResourceTokenBrokerUrl = "https://medwebapi.azurewebsites.net";
